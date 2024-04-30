857ab1841c25a1db9db0b568b133c8e8a68ca565
Supreme Kandel <sandeshkandel2025@gmail.com>
2024-04-30 11:47:50 +0545

    Sandesh kandel just created a simple python program

---
+++ conditionals.py
@@ -0,0 +1,8 @@
+age = int(input("Enter your age: "))
+day = 'Sunday'
+if age<14:
+    print("You are a child")
+elif age>14 and age<28:
+    print("You' are an adult")
+else:
+    print("you're old")
