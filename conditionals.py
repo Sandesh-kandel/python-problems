1e9e607e90f3f0084d2808feb209562d4c9fb2a6
Supreme Kandel <sandeshkandel2025@gmail.com>
2024-04-30 12:08:45 +0545

    New version is updted where if user enters non integer it asks user to enter a vlaid numebr

--- conditionals.py
+++ conditionals.py
@@ -1,8 +1,24 @@
-age = int(input("Enter your age: "))
-day = 'Sunday'
-if age<14:
-    print("You are a child")
-elif age>14 and age<28:
-    print("You' are an adult")
+# age = int(input("Enter your age: "))
+# day = 'Sunday'
+# if age<14:
+#     print("You are a child")
+# elif age>14 and age<28:
+#     print("You' are an adult")
+# else:
+#     print("you're old")
+
+# what if users enters a neagtive age
+
+while True:
+    age_input = (input("ENter your age"))
+    if age_input.isdigit():
+        age= int(age_input)
+        if age>=0:
+            break
+    print('Print enter a valid age.')
+if age < 14:
+    print("Child") 
+elif age >14 and age <29: 
+    print("Adult")
 else:
-    print("you're old")
+    print("You are old")              
